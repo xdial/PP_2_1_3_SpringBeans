cb28440325eb3879c052aa270c979e0702ed9ffa
xdial <xdial@mail.ru>
2022-08-15 13:29:06 +0300

    Добавлен класс Dog

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,17 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "cat")
+    public Cat getCat(){
+        return new Cat();
+    }
+
 }
